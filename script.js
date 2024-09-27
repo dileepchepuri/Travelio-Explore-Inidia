5f44fa64b87cede873b7f5a0633965bbc2ea8309
dileepchepuri <167414876+dileepchepuri@users.noreply.github.com>
2024-09-27 16:58:20 +0530

    The source code of Travelio

---
+++ script.js
@@ -0,0 +1,23 @@
+const preloader=document.querySelector("[data-preloader]");
+window.addEventListener("load",()=>{
+    preloader.classList.add("remove");
+})
+const addEventonElements=function(elements,eventType,callback){
+    for (let i=0,len=elements.length;i<len;i++){
+        elements[i].addEventListener(eventType,callback);
+    }
+}
+const navbar=document.querySelector("[data-navbar]");
+const navTogglers=document.querySelectorAll("[data-nav-toggler]");
+const overlay=document.querySelector("[data-overlay]");
+const toggleNav=function(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+addEventonElements(navTogglers,"click",toggleNav);
+// Headers.
+const Header=document.querySelector("[data-header]");
+window.addEventListener("scroll",function(){
+    Header.classList[window.scrollY>100?"add":"remove"]("active");
+})
